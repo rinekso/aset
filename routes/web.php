d790f6e02fcbf09a232ff6a71fefdafd2f6913a2
Eka Anre Sangsya <ekaanresangsya@gmail.com>
2018-03-26 05:18:31 +0700

    fix KIR

--- routes/web.php
+++ routes/web.php
@@ -105,7 +105,7 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('/data-user-unit', 'BidangController@dataUserUnit')->name('.dataUserUnit');
         Route::get('/aktif/{id}', 'BidangController@aktif')->name('.aktif');
         Route::get('/nonaktif/{id}', 'BidangController@nonaktif')->name('.nonaktif');
-        ROute::get('store-lokasi', 'BidangController@storelokasi');
+        ROute::get('store-lokasi', 'BidangController@storelokasi')->name('/storelokasi');
         Route::post('store-lokasi', 'BidangController@storelokasi')->name('/storeLokasi');
     });  
 });
